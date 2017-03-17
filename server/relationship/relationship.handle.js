fc2fd48d2df3a54e083fd7348b8bccc5e2829e93
Simon Zachau <zachau@in.tum.de>
2017-03-17 12:12:29 +0100

    filterCorefs function

--- server/relationship/relationship.handle.js
+++ server/relationship/relationship.handle.js
@@ -6,26 +6,16 @@ const StanfordCoreNLPClient = require('../middelwares/StanfordCoreNLPClient');
 var jsonQuery = require('json-query');
 
 var jsonObj = JSON.parse(fs.readFileSync(path.join(__dirname, '../resources') + '/relationships-testData.json', 'utf8'));
-
-var testComposer = "Bach 's";
-var data = jsonQuery('corefs[**][*text = ' + testComposer + ']', {
-    data: jsonObj
-});
-
-// get coref keys
-var corefKeys = filterCorefKeys(jsonObj, testComposer);
+var testComposer = "Bach";
 
 // get corefs
-var corefs = corefKeys.map(key => jsonObj.corefs[key]);
-
-// flatten array
-corefs = [].concat.apply([], corefs);
+var corefs = filterCorefs(jsonObj, testComposer);
+console.log(corefs);
 
 // now find the relations
-console.log(corefs);
 corefs.forEach(function(coref) {
 
-})
+});
 
 
 /* GET home page. */
@@ -36,23 +26,31 @@ router.get('/', function (req, res, next) {
 
 module.exports = router;
 
-// get the coref ids for the given search text.
-function filterCorefKeys(jsonObj, searchText) {
+// get the corefs for the given search text
+function filterCorefs(jsonObj, searchText) {
+    // 1) get corefs keys
     var data = jsonQuery('corefs[**][*text = ' + searchText + ']', {
         data: jsonObj
     });
-    var corefValues = Object.values(jsonObj.corefs).map(x => x.length);
+    var corefsValuesLengths = Object.values(jsonObj.corefs).map(x => x.length);
     var corefKeys = [];
-    data.key.forEach(function(key) {
-        var index = 0;
-        while (key + 1 > corefValues[index]) {
-            key -= corefValues[index];
-            index++;
+    data.key.forEach(function(objectIndex) {
+        var corefKeyIndex = 0;
+        while (objectIndex + 1 > corefsValuesLengths[corefKeyIndex]) {
+            objectIndex -= corefsValuesLengths[corefKeyIndex];
+            corefKeyIndex++;
         }
-        var corefKey = Object.keys(jsonObj.corefs)[index];
+        var corefKey = Object.keys(jsonObj.corefs)[corefKeyIndex];
         if (corefKeys.indexOf(corefKey) == -1) {
             corefKeys.push(corefKey);
         }
     });
-    return corefKeys;
+
+    // 2) get corefs
+    var corefs = corefKeys.map(key => jsonObj.corefs[key]);
+
+    // 3) flatten array
+    corefs = [].concat.apply([], corefs);
+
+    return corefs;
 }
